3233db53632ad6cfb234d27d2708273569220675
kalik <nAsad1385@gmail.com>
2022-07-05 00:22:31 +0300

    Done x2

--- app/v1/api.py
+++ app/v1/api.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
-from app.v1.endpoints.tasks import router as users_router
+from app.v1.endpoints.tasks import router as tasks_router
 
 
 router = APIRouter()
-router.include_router(users_router, prefix='/tasks', tags=['Tasks'])
+router.include_router(tasks_router, prefix='/tasks', tags=['Tasks'])
